c1d602e18d0cb2f77b9f751add1409820193effb
tennoken <tennoken@naver.com>
2022-09-01 16:56:14 +0900

    make TodoList with redux

--- src/index.js
+++ src/index.js
@@ -9,20 +9,68 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 
+const addTodo = (text) => {
+  return {
+    type : ADD_TODO,
+    text
+  }
+}
+
+const deleteTodo = (id) => {
+  return {
+    type : DELETE_TODO,
+    id
+  }
+}
+
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch(action.type){
     case ADD_TODO:
-      return [];
+      return [...state, {text : action.text, id : Date.now()}];
     case DELETE_TODO:
-      return [];
+      return state.filter(item => item.id !== action.id);
     default:
       return state;
   }
 }
 
+const displayTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = "";
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = "Delete";
+    btn.addEventListener('click', (e) => {
+      
+      dispatchDeleteTodo(e);
+    })
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
+}
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.subscribe(displayTodos);
+
+
 
 
 const onSubmit = (e) => {
@@ -30,7 +78,7 @@ const onSubmit = (e) => {
   console.log(input.value);
   const todos = input.value;
   input.value = "";
-  store.dispatch({type: ADD_TODO, text : todos});
+  dispatchAddTodo(todos);
 }
 
 
